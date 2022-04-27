acd238f6426add6020d92723c06ea0b86cd0667f
seryl lounis <seryllounis@gmail.com>
2022-04-27 22:03:16 +0200

    add dropdown on flat page

--- src/components/Flat.js
+++ src/components/Flat.js
@@ -3,31 +3,54 @@ import { useParams } from 'react-router-dom'
 import logements from '../data/logements.json'
 import Carousel from './Carousel'
 import '../styles/flat.scss'
+import Tag from './Tag'
+import DropDown from './DropDown'
+import Footer from "./Footer"
+
 export default function Flat() {
+  const [flat, setFlat] = useState(null)
   const {id} = useParams()
-  const [flat, setFlat] = useState({})
+
   useEffect(() => {
     let current = logements.filter(flat => flat.id === id)
     setFlat(current[0])
-  }, [])
-  console.log(flat)
+  }, [flat])
+
   return (
-    <div className='flat-page'>
-      {flat && (
-        <>
+    <>
+      <div className='flat-page'>
+        {flat && (
+          <>
           <Carousel images={flat.pictures}/>
+          <div className="flat-header">
+            <div className="flat-title">
+              <h3>{flat.title}</h3>
+              <p>{flat.location}</p>
+            </div>
+            <div className="flat-host">
+              <p>{flat.host.name}</p>
+              <img src={flat.host.picture} alt={flat.host.name} />
+            </div>
+          </div>
+          <div className="flat-attr">
+            <div className="tags">
+              {flat.tags.map((tag, index) => (
+                <Tag key={index}>{tag}</Tag>
+              ))}
+            </div>
+          </div>
+          <div className="flat-desc">
+            <DropDown title="Description">{flat.description}</DropDown>
+            <DropDown title="Equipements">
+              <ul>
+                {flat.equipments.map((equipment, index) => <li key={index}>{equipment}</li>)}
+              </ul>
+            </DropDown>
+          </div>
         </>
-      )}
-      <div className="flat-header">
-        <div className="flat-title">
-          <h3>{flat.title}</h3>
-          <p>{flat.location}</p>
-        </div>
-        <div className="flat-host">
-          <p>{flat.host.name}</p>
-          <img src={flat.host.picture} alt={flat.host.name} />
-        </div>
+        )}
       </div>
-    </div>
+      <Footer />
+    </>
   )
 }
--- src/components/Tag.js
+++ src/components/Tag.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import "../styles/tag.scss"
 
 export default function Tag({children}) {
   return (
